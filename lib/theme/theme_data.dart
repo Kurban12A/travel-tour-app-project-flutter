a19317e67d78082e573188943a537c46e11d6ba5
Kurban12A <manrulez@mail.ru>
2023-09-10 23:48:15 +0400

    add auth widget 10-09-2023

--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 // Kurban 09-09-2023
 //переменная которая будет хранить тему нашего приложения
 final themeData = ThemeData(
+  appBarTheme: const AppBarTheme(
+    color: Colors.white,
+  ),
   scaffoldBackgroundColor: Colors.white,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
   useMaterial3: true,
 );
--- lib/views/auth.dart
+++ lib/views/auth.dart
@@ -1,12 +1,93 @@
+
 import 'package:flutter/material.dart';
 
+
+// Kurban 10-09-2023
 class AuthScreenWidget extends StatelessWidget {
   const AuthScreenWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('2 creen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            _AuthAppBarTextWidget(),
+            _AuthOutlineButtonWidget()
+          ],
+        ),
+      ),
+      body: const Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 5, vertical: 5,
+                ),
+                child: Text('ArmeniaTour', 
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold
+                ),
+                textAlign: TextAlign.start,),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('DISCOVER AMAZING PLACES', textAlign: TextAlign.center,),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
+
+
+// виджет отвечающий за AppBar кнопку выбора языка
+class _AuthOutlineButtonWidget extends StatelessWidget {
+  const _AuthOutlineButtonWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 20, vertical: 10)),
+        foregroundColor: MaterialStateProperty.all(Colors.blue)
+      ),
+      child: const Text('English'),
+      onPressed: (){},
+      );
+  }
+}
+
+
+// виджет отвечающий за текст в appbar
+class _AuthAppBarTextWidget extends StatelessWidget {
+  const _AuthAppBarTextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: Text('Sign in',
+      textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold),
+          ),
+    );
+  }
+}
+
+
